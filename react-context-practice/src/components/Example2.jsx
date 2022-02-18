6497468f95c668b387170a4d3ab3a16db1b44e69
bongjunk <mydlwm_s@naver.com>
2022-02-18 17:48:00 +0900

    context 여러 사용법

---
+++ react-context-practice/src/components/Example2.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import PersonContext from "../contexts/PersonContext";
+
+export default class Example2 extends React.Component {
+  // static contextType = PersonContext;
+  render() {
+    const persons = this.context;
+    return (
+      <div>
+        <ul>
+          {persons.map((person) => (
+            <li>{person.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+Example2.contextType = PersonContext;
